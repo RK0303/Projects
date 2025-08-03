d4a8562fca5d539cfccce5d4c36494a9b8529552
RK0303 <ruthish3@gmail.com>
2025-08-03 12:17:15 +0530

    Responsive  website

---
+++ Fried chicken website/js/main.js
@@ -0,0 +1,67 @@
+// SHOW MENU
+const navMenu = document.getElementById('nav_menu'),
+      navToggle = document.getElementById('nav_toggle'),
+      navClose = document.getElementById('nav_close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show_menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show_menu')
+    })
+}
+
+//  REMOVE MENU MOBILE 
+const navLink = document.querySelectorAll('.nav_link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav_menu')
+    // When we click on each nav_link, we remove the show_menu class
+    navMenu.classList.remove('show_menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// ADD SHADOW HEADER
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+// SHOW SCROLL UP
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scrollup')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show_scroll')
+						: scrollUp.classList.remove('show_scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active_link')
+		}else{
+			sectionsClass.classList.remove('active_link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
